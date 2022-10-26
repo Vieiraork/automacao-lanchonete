a39e2afa2457aae072028e2c6f3ab54f187d4cca
Vieiraork <vieiraork@gmail.com>
2022-10-26 14:56:46 -0300

    Add - combo controller

--- controller/comboController.php
+++ controller/comboController.php
@@ -1,7 +1,16 @@
 <?php
+
+require_once "service/comboService.php";
+
 class ComboController {
-    public function decrementItem() {
-        // TODO : IMPLEMENT CODE HERE
+    public function __construct() {
+        $this->service = new ComboService;
+    }
+
+    public function decrementItem($tag, $conn) {
+        $resposta = $this->service->decrementSingleItem($tag, $conn);
+
+        return $resposta;
     }
 }
 
