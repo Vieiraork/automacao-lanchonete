1be1198f6e4e9938188051d086e7d28bd9c5248d
Vieiraork <vieiraork@gmail.com>
2022-10-26 14:59:58 -0300

    Add - service folder for service classes

--- service/comboService.php
+++ service/comboService.php
@@ -1,40 +1,43 @@
 <?php
 
-class Combo {
+class ComboService {
     function index() {
-
+        $query = 'SELECT * FROM combos';
     }
     
     function create($combo) {
-        
+        $query = "INSERT INTO combos (id_combo, cd_combo, dt_criacao, qtd_combo) VALUES ('$combo->getIdCombo()','$combo->getCdCombo()','$combo->getDtCriacao()','$combo->getQtdCombo())";
+
     }
     
     function edit($id, $combo) {
-    
+        $query = "UPDATE combos SET cd_combo = '$combo->getCdCombo()', qtd_combo = '$combo->getQtdCombo()', dt_alteracao = '$combo->getDtAlteracao()'";
     }
     
     function destroy($id) {
-    
+        $query = "DELETE combos WHERE id_combo = '$id'";
     }
     
     function decrementSingleItem($tag, $conn) {
-        if ($conn) {
-            echo $tag;
-        } else {
-            echo "Não há conexão com o banco de dados";
+        $query = 'SELECT qtd_combo FROM combos WHERE cd_combo = '. $tag;
+        $qtd = mysqli_query($conn, $query);
+        
+        $row = mysqli_fetch_array($qtd);
+        $row = json_encode($row);
+        $novo = json_decode($row);
+
+        if ($novo->qtd_combo == 0) {
+            return "Infelizmente o combo solicitado não possui mais itens";
         }
-        
-        // $conn = $this->conn->stablishDBConnection();
-    
-        // $query = 'SELECT qtd_itens FROM Lanchonete';
-        // $result = $conn->query($query);
-    
-        // if (!is_null($result) || $result < 3) {
-        //     // TODO - ADICIONAR FUNÇÃO DE AVISO POR API DO WHATS OU TELEGRAM
-        // } else {
-        //     // TODO - ADICIONAR PROSSEGUIMENTO AO PEDIDO
-        // }
-        
+
+        $nova_qtd = ($novo->qtd_combo - 1);
+
+        $query = "UPDATE combos SET qtd_combo = ". $nova_qtd ." WHERE cd_combo = ". $tag;
+        if ($conn->query($query)) {
+            return "Seu pedido foi recebido";
+        } else {
+            return "Sem itens para completar seu pedido, por favor, verifique outro produto";
+        }
     }
 }
 
--- service/usuarioService.php
+++ service/usuarioService.php
@@ -13,16 +13,24 @@ class Usuario {
     
     }
     
-    public function create() {
-    
+    public function create($conn, $usuario) {
+        $query = "INSERT INTO usuario (no_usuario, senha, dt_registro) 
+        VALUES ('$usuario->getNoUsuario()', '$usuario->getSenha()', '$usuario->getDtRegistro()')";
     }
     
     public function edit($id, $usuario) {
-    
+        $query = "UPDATE usuario SET no_usuario = '$usuario->getNoUsuario()', 
+        senha = '$usuario->getSenha()', dt_alteracao = '$usuario->getDtAlteracao()' 
+        WHERE id_usuario = '$usuario->getIdUsuario()'";
     }
     
     public function destroy($id) {
-        
+        $query = "DELETE usuario WHERE id_usuario = '$id'";
+    }
+
+    public function perfil($id) {
+        $query = "SELECT no_usuario, dt_registro, dt_alteracao FROM usuario WHERE id_usuario = '$id'";
+
     }
 }
 
