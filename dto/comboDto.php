8284510d9175398a031fea9b283944dd12433331
Vieiraork <vieiraork@gmail.com>
2022-10-26 14:57:39 -0300

    Add - dto folder to dto classes

--- dto/comboDto.php
+++ dto/comboDto.php
@@ -1,7 +1,51 @@
 <?php
 
 class ComboDto {
+    private $id_combo;
+    private $cd_combo;
+    private $dt_criacao;
+    private $dt_alteracao;
+    private $qtd_combo;
 
+    public function getIdCombo() {
+        return $id_combo;
+    }
+
+    public function setIdCombo($id_combo) {
+        $this->id_combo = $id_combo;
+    }
+
+    public function getCdCombo() {
+        return $cd_combo;
+    }
+
+    public function setCdCombo($cd_combo) {
+        $this->cd_combo = $cd_combo;
+    }
+
+    public function getDtCriacao() {
+        return $dt_criacao;
+    }
+
+    public function setDtCriacao($dt_criacao) {
+        $this->dt_criacao = $dt_criacao;
+    }
+
+    public function getDtAlteracao() {
+        return $dt_alteracao;
+    }
+
+    public function setDtAlteracao($dt_alteracao) {
+        $this->dt_alteracao = $dt_alteracao;
+    }
+
+    public function getQtdCombo() {
+        return $qtd_combo;
+    }
+
+    public function setQtdCombo($qtd_combo) {
+        $this->qtd_combo = $qtd_combo;
+    }
 }
 
 
--- dto/userDto.php
+++ dto/userDto.php
@@ -1,7 +1,51 @@
 <?php
 
 class UserDto {
-    // TODO - ACRESCENTAR OS ATRIBUTOS PARA O OBJETO DE TRANSFERÊNCIA
+    private $id_usuario;
+    private $no_usuario;
+    private $senha;
+    private $dt_registro;
+    private $dt_alteracao;
+
+    public function getIdUsuario() {
+        return $id_usuario;
+    }
+
+    public function setIdUsuario($id_usuario) {
+        $this->id_usuario = $id_usuario;
+    }
+
+    public function getNoUsuario() {
+        return $id_usuario;
+    }
+
+    public function setNoUsuario($no_usuario) {
+        $this->no_usuario = $no_usuario;
+    }
+
+    public function getSenha() {
+        return $senha;
+    }
+
+    public function setSenha($senha) {
+        $this->senha = $senha;
+    }
+
+    public function getDtRegistro() {
+        return $dt_registro;
+    }
+
+    public function setDtRegistro($dt_registro) {
+        $this->dt_registro = $dt_registro;
+    }
+
+    public function getDtAlteracao() {
+        return $dt_alteracao;
+    }
+
+    public function setDtAlteracao($dt_alteracao) {
+        $this->dt_alteracao = $dt_alteracao;
+    }
 }
 
 ?>
