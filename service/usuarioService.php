c2773f515e7dc3f9d4a1f6664a663921e75f7181
Vieiraork <vieiraork@gmail.com>
2022-11-06 20:28:53 -0300

    Change - some methods to user service

--- service/usuarioService.php
+++ service/usuarioService.php
@@ -1,27 +1,66 @@
 <?php
 
-require_once 'connection/connection.php';
+require_once '../connection/connection.php';
 
-class Usuario {
+class UsuarioService {
+    public function __construct() {
+        $this->conn = new DbConnection;
+    }
 
+    public function login($email, $senha) {
+        $query = "SELECT * FROM usuario WHERE email = ".$email." senha = ".$senha;
+        $usuario = mysqli_query($this->conn->stablishDBConnection(), $query);
 
-    public function login() {
+        $novo_usuario_logado = mysqli_fetch_array($usuario);
+        if (is_null($novo_usuario_logado)) {
+            header('Location: ../index.php');
+        } else {
+            session_start();
+            $_SESSION["usuario"] = $novo_usuario_logado;
+            echo $_SESSION[$novo_usuario_logado];
+        }        
 
+        echo "Email: ".$email." ,Senha: ".$senha;
     }
     
     public function logout() {
     
     }
     
-    public function create($conn, $usuario) {
-        $query = "INSERT INTO usuario (no_usuario, senha, dt_registro) 
-        VALUES ('$usuario->getNoUsuario()', '$usuario->getSenha()', '$usuario->getDtRegistro()')";
+    public function create($usuario) {
+        $query = "INSERT INTO usuario (no_usuario, senha, email, dt_registro) 
+        VALUES ('$usuario->getNoUsuario()', '$usuario->getSenha()', '$usuario->getEmail()', '$usuario->getDtRegistro()')";
+        $retorno = null;
+
+        try {
+            mysqli_query($this->conn->stablishDBConnection(), $query);
+            $retorno = true;
+        } catch (\Throwable $th) {
+            $returon = false;
+            throw $th;
+        }
+
+        if ($retorno) {
+            header('Location: ../views/home.php');
+        }
     }
     
     public function edit($id, $usuario) {
         $query = "UPDATE usuario SET no_usuario = '$usuario->getNoUsuario()', 
         senha = '$usuario->getSenha()', dt_alteracao = '$usuario->getDtAlteracao()' 
         WHERE id_usuario = '$usuario->getIdUsuario()'";
+        $retorno = null;
+
+        try {
+            $retorno = true;
+        } catch (\Throwable $th) {
+            $retorno = false;
+            throw $th;
+        }
+
+        if ($retorno) {
+            // retornar para alguma página
+        }
     }
     
     public function destroy($id) {
