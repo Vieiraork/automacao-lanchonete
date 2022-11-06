e2134b48148a3e87e41109798073db911688de00
Vieiraork <vieiraork@gmail.com>
2022-11-06 20:27:33 -0300

    Change - made index to login

--- index.php
+++ index.php
@@ -1,12 +1,65 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
     <title>Home</title>
+    <style>
+        .content {
+            background-color: #f0f0f0;
+        }
+
+        .content-header {
+            margin-top: 2rem;
+        }
+    </style>
 </head>
-<body>
-    
+
+<body class="content">
+    <section class="content-header">
+        <div class="container">
+            <div class="card card-outline card-primary">
+                <div class="card-header">
+                    <div class="card-title">Login</div>
+                </div>
+                <div class="card-body">
+                    <form action="controller/loginController.php" method="post">
+                        <div class="form-group">
+                            <label for="e-mail">E-mail</label>
+                            <input type="text" name="email" id="email" class="form-control">
+                        </div>
+                        <div class="form-group">
+                            <label for="senha">Senha</label>
+                            <input type="password" name="senha" id="senha" class="form-control">
+                        </div>
+                        <div class="row">
+                            <div class="col-md-12">
+                                <div class="form-group float-right">
+                                    <a href="views/register.php" class="cadastro">Criar conta</a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-md-12">
+                                <div class="form-group float-right">
+                                    <input type="reset" value="Limpar" class="btn btn-light">
+                                    <input type="submit" value="Entrar" class="btn btn-success">
+                                </div>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+
+            </div>
+        </div>
+    </section>
+
+
+    <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-Fy6S3B9q64WdZWQUiU+q4/2Lc9npb8tCaSX9FK7E8HnRr0Jz8D6OP9dO5Vg3Q9ct" crossorigin="anonymous"></script>
 </body>
+
 </html>
